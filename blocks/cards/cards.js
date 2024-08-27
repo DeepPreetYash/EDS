243c8bc8afb4397ce87d463f43e05b0d1cbf2f50
Deepti <deepti.baijal@gmail.com>
2024-08-27 10:03:10 +0530

    code for frankiln project POC

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,11 +1,19 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
+import { createOptimizedPicture,fetchPlaceholders } from '../../scripts/aem.js';
 
-export default function decorate(block) {
+export default async function decorate(block) {
+  const placeholders = await fetchPlaceholders();
+  const { fkey } = placeholders;
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
+    //code to create div and placeholder
+    
+    const newDiv = document.createElement('div');
+    const textContent = document.createTextNode(fkey);
+    newDiv.appendChild(textContent);
+    li.append(newDiv);
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
